cc0dc79a36c3070e62035375233b275aca9223f7
Brian Blosser <bpb54321@gmail.com>
2019-07-12 14:12:46 -0400

    Moved the schema into schema.graphql.

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga');
 let { testDialogs } = require('./data');
 
-const typeDefs = `
-  type Query {
-    dialogs: [Dialog!]!
-  }
-
-  type Dialog {
-    id: ID!
-    name: String!
-    roles: [Role!]!
-    lines: [Line!]!
-  }
-
-  type Role {
-    id: ID!
-    name: String!
-  }
-
-  type Line {
-    id: ID!
-    text: String!
-    guess: String!
-    role: Role!
-    number: Int!
-  }
-`;
 
 const resolvers = {
   Query: {
     dialogs: () => testDialogs,
   },
-  Dialog: {
-    id: (parent) => parent.id,
-    name: (parent) => parent.name,
-    roles: (parent) => parent.roles,
-    lines: (parent) => parent.lines,
-  }
 };
 
 const server = new GraphQLServer({

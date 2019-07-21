2407dbf355247b822ce045987f63fbbe12260d7f
Brian Blosser <bpb54321@gmail.com>
2019-07-21 11:38:52 -0400

    Added a query for a single dialog.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,9 @@
 const {getUserId} = require("../utils");
 
+exports.dialog = async function(root, args, context, info) {
+  return await context.prisma.dialog({id: args.id});
+};
+
 exports.dialogs = async function(root, args, context, info) {
   const userId = getUserId(context);
   return await context.prisma.user({id: userId}).dialogs();

ff337c91d945ee663a9773642387b9f92a1c5064
Brian Blosser <bpb54321@gmail.com>
2019-07-22 16:55:29 -0400

    Make createDialog() just create a Dialog without any Roles or Lines.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -5,30 +5,8 @@ const { APP_SECRET, getUserId } = require('../utils');
 exports.createDialog = async function(root, args, context, info) {
   const userId = getUserId(context);
 
-  // Create lines and roles first
-  // Can use the create keyword to do nested creation with Prisma
-  // However, appears that you can only do one level of nested writes with
-  // Prisma, so we do the lines first, then the dialogs
-  // https://www.prisma.io/docs/1.34/prisma-client/basic-data-access/writing-data-JAVASCRIPT-rsc6/#nested-object-writes
-  let linesWithCreatedRole = args.lines.map((line) => {
-    const {text, guess, number, role} = line;
-    const lineWithCreatedRole = {
-      text,
-      guess,
-      number,
-      role: {
-        create: role,
-      }
-    };
-
-    return lineWithCreatedRole;
-  });
-
   let dialog = {
     name: args.name,
-    lines: {
-      create: linesWithCreatedRole,
-    },
     user: {
       connect: {
         id: userId

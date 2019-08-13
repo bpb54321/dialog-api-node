a18986220d6ff058fc96811956a7f4180d1885e1
Brian Blosser <bpb54321@gmail.com>
2019-08-13 17:49:19 -0400

    Updated the test to make sure all line fields were updated.

--- cypress/integration/update-line.js
+++ cypress/integration/update-line.js
@@ -20,14 +20,20 @@ context('updateLine', () => {
       {
         "id": "cjz9xb4e002rb0766w1kdlgw5",
         "number": 4,
+        "text": "This is the text for line 4.",
+        "roleId": "cjz9xb3t202r3076635xxmoqm",
       },
       {
         "id": "cjz9xb58s02rj0766m18izy5c",
         "number": 5,
+        "text": "This is the text for line 5.",
+        "roleId": "cjz9xb3i102qw0766ubr6dvc3",
       },
       {
         "id": "cjz9xb60m02rr0766hzo76vqf",
         "number": 6,
+        "text": "This is the text for line 6.",
+        "roleId": "cjz9xb3t202r3076635xxmoqm",
       }
     ];
     // This includes the fields that we want to use to update the lines
@@ -48,8 +54,12 @@ context('updateLine', () => {
           `
             mutation UpdateLine($lines: [LineInput!]!) {
               updateLine(lines: $lines) {
-                id,
-                number,
+                id
+                number
+                text
+                role {
+                  id
+                }
               }
             }
           `,
@@ -57,7 +67,32 @@ context('updateLine', () => {
     }).should((response) => {
       expect(response.body).to.deep.equal({
         "data": {
-          "updateLine": linesWithDesiredFields,
+          "updateLine": [
+            {
+              id: linesWithDesiredFields[0].id,
+              number: linesWithDesiredFields[0].number,
+              text: linesWithDesiredFields[0].text,
+              role: {
+                id: linesWithDesiredFields[0].roleId
+              }
+            },
+            {
+              id: linesWithDesiredFields[1].id,
+              number: linesWithDesiredFields[1].number,
+              text: linesWithDesiredFields[1].text,
+              role: {
+                id: linesWithDesiredFields[1].roleId
+              }
+            },
+            {
+              id: linesWithDesiredFields[2].id,
+              number: linesWithDesiredFields[2].number,
+              text: linesWithDesiredFields[2].text,
+              role: {
+                id: linesWithDesiredFields[2].roleId
+              }
+            },
+          ],
         }
       });
     })
